a6a8ec10fc2ee9d70c101c759aa80ebd396e96c4
Maryna Longnickel <37321032+MarynaLongnickel@users.noreply.github.com>
2021-12-01 10:51:12 -0600

    Update day1.py

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,5 +1,15 @@
 from urllib.request import urlopen
 
 data = urlopen('https://raw.githubusercontent.com/MarynaLongnickel/AdventOfCode2021/master/Day1/day1.txt').read().decode().split('\n')[:-1]
+data = [int(x) for x in data]
 
-sum([int(y) > int(x) for x, y in zip(data, data[1:])])
+sum([y - x for x, y in zip(data, data[1:])])
+
+# -------------------------------------------------- Part 2 --------------------------------------------------------
+
+t = 0
+
+for i in range(1, len(data) - 2):
+    t += sum(data[i : i + 3]) > sum(data[i - 1 : i + 2])
+    
+t
