d474f3a6753cdfef0a49825dc24818fd9146ab88
Maryna Longnickel <37321032+MarynaLongnickel@users.noreply.github.com>
2021-12-07 12:24:06 -0600

    Update day7.py

--- Day7/day7.py
+++ Day7/day7.py
@@ -3,35 +3,37 @@ from urllib.request import urlopen
 data = urlopen('https://raw.githubusercontent.com/MarynaLongnickel/AdventOfCode2021/main/Day7/day7.txt').read().decode().split('\n')[:-1]
 data = sorted(map(int, data[0].split(',')))
 
+def s1(i): return sum([abs(x - i) for x in data])
+def s2(i): return sum([abs(x - i) * (abs(x - i) + 1) / 2 for x in data])
+
 p = list(set(data))
 m = data[int(len(data) / 2)]
-
-t = sum([abs(x - m) for x in data])
+t = s1(m)
 
 for i in range(m + 1, 1000):
-    s = sum([abs(x - p[i]) for x in data])
+    s = s1(p[i])
     if s > t: break
     else: t = s
-        
+
 for i in range(m - 1, 0, -1):
-    s = sum([abs(x - p[i])  for x in data])
+    s = s1(p[i])
     if s > t: break
     else: t = s
-        
+    
 t
 
 # -------------------------------------------------- Part 2 ------------------------------------------------------
 
-t = sum([abs(x - m) * (abs(x - m) + 1) / 2 for x in data])
+t = s2(m)
 
 for i in range(m + 1, 1000):
-    s = sum([abs(x - p[i]) * (abs(x - p[i]) + 1) / 2 for x in data])
+    s = s2(p[i])
     if s > t: break
     else: t = s
-        
+
 for i in range(m - 1, 0, -1):
-    s = sum([abs(x - p[i]) * (abs(x - p[i]) + 1) / 2  for x in data])
+    s = s2(p[i])
     if s > t: break
     else: t = s
-        
+    
 t
